05c729edd38c78bcf5d0aa8fe7f54d80d7594667
Trager LeMaire <142815456+TragerSL2004@users.noreply.github.com>
2024-10-17 08:56:41 -0500

    Index buffers completed

--- OpenGLTest/src/Application.cpp
+++ OpenGLTest/src/Application.cpp
@@ -108,13 +108,15 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << '\n';
 
     float positions[] = {
-       -0.5f, -0.5,
-        0.5f,  0.5,
-        0.5f, -0.5,
-
+       -0.5f, -0.5f,
+        0.5f, -0.5f,
         0.5f,  0.5f,
-       -0.5,   0.5f,
-       -0.5f, -0.5f
+       -0.5f,  0.5f
+    };
+    
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
     };
 
     unsigned int buffer;
@@ -124,6 +126,11 @@ int main(void)
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * 2 * sizeof(float), indices, GL_STATIC_DRAW);
 
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
@@ -135,7 +142,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
