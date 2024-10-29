33db06084e03b06d612310dac7124169d61ede5a
Trager LeMaire <142815456+TragerSL2004@users.noreply.github.com>
2024-10-29 11:42:36 -0500

    Cached uniform location for optimization

--- OpenGLTest/src/Shader.h
+++ OpenGLTest/src/Shader.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <unordered_map>
 
 struct ShaderProgramSource
 {
@@ -13,6 +14,7 @@ class Shader
 private:
 	std::string m_FilePath;
 	unsigned int m_RendererID;
+	std::unordered_map<std::string, unsigned int> m_UniformLocationCache;
 	unsigned int CompileShader(unsigned int type, const std::string& source);
 	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
 	ShaderProgramSource ParseShader(const std::string& filepath);
